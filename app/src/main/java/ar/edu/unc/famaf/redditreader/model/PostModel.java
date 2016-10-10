6fc2c18d8c918e1e8a8caad4e6a473c92252dddd
aguilarjp <aguilarjp.95@gmail.com>
2016-10-10 19:29:54 -0300

    Add a post model

--- app/src/main/java/ar/edu/unc/famaf/redditreader/model/PostModel.java
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/model/PostModel.java
@@ -1,5 +1,60 @@
 package ar.edu.unc.famaf.redditreader.model;
 
 
+import java.util.Date;
+
 public class PostModel {
+    private String mTitle;
+    private String mAuthor;
+    private Date mDate;
+    private int mCommentNumber;
+    private String mThumbnail; // This is the image URL
+
+    public PostModel(String title, String author, Date date, int commentNumber, String thumbnail) {
+        mTitle = title;
+        mAuthor = author;
+        mDate = date;
+        mCommentNumber = commentNumber;
+        mThumbnail = thumbnail;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public void setAuthor(String author) {
+        mAuthor = author;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public int getCommentNumber() {
+        return mCommentNumber;
+    }
+
+    public void setCommentNumber(int commentNumber) {
+        mCommentNumber = commentNumber;
+    }
+
+    public String getThumbnail() {
+        return mThumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        mThumbnail = thumbnail;
+    }
 }
