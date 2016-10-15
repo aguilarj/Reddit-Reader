c843cbcdd1d3a0ad587200147350c51c46fc7f5e
aguilarjp <aguilarjp.95@gmail.com>
2016-10-15 16:37:53 -0300

    Adapt example posts to last change

--- app/src/main/java/ar/edu/unc/famaf/redditreader/backend/Backend.java
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/backend/Backend.java
@@ -27,6 +27,7 @@ public class Backend {
         p1.setAuthor("bengaldude545");
         p1.setThumbnail("https://a.thumbs.redditmedia.com/0e0cKrikx0u2EeCT6Di9LgLXrE2WVICdJpAw1Tj4Ka8.jpg");
         p1.setCommentNumber(1186);
+        p1.setId("57m9dh");
 
         // Configuring date for this particular post
         calendar.set(Calendar.YEAR, 2016);
@@ -44,6 +45,7 @@ public class Backend {
         p2.setAuthor("rahul8aggarwal");
         p2.setThumbnail("https://a.thumbs.redditmedia.com/1xJJxofAvAglF0XsQzx0z-kI-NcXf62wLaH5VLkEcc0.jpg");
         p2.setCommentNumber(887);
+        p2.setId("57j3nf");
 
         // Configuring date for this particular post
         calendar.set(Calendar.YEAR, 2016);
@@ -61,6 +63,7 @@ public class Backend {
         p3.setAuthor("el_matto");
         p3.setThumbnail("https://b.thumbs.redditmedia.com/L_P-6sg6mPZ36PW0cN00tEx62W6aeyYqjH9hdMTLyos.jpg");
         p3.setCommentNumber(1154);
+        p3.setId("57jwjr");
 
         // Configuring date for this particular post
         calendar.set(Calendar.YEAR, 2016);
@@ -78,6 +81,7 @@ public class Backend {
         p4.setAuthor("SlimJones123");
         p4.setThumbnail("https://b.thumbs.redditmedia.com/_gxcq7uLcFHkAmCvBf9hLQ-wMDMgTYBuXtRGZsixL2w.jpg");
         p4.setCommentNumber(1215);
+        p4.setId("57kp5u");
 
         // Configuring date for this particular post
         calendar.set(Calendar.YEAR, 2016);
@@ -95,6 +99,7 @@ public class Backend {
         p5.setAuthor("lacajadeldiablo");
         p5.setThumbnail("https://b.thumbs.redditmedia.com/fAS3pQnydmpsjIM3MuH9sDNVrF3e4WbwVN9moMaoR3w.jpg");
         p5.setCommentNumber(735);
+        p5.setId("57l15n");
 
         // Configuring date for this particular post
         calendar.set(Calendar.YEAR, 2016);
@@ -112,6 +117,7 @@ public class Backend {
         p6.setAuthor("lacajadeldiablo");
         p6.setThumbnail("https://b.thumbs.redditmedia.com/h74JWprM3wljpdBOOpKDxt5sdZWPRtJBVULIobFfCBU.jpg");
         p6.setCommentNumber(735);
+        p6.setId("57kkpz");
 
         // Configuring date for this particular post
         calendar.set(Calendar.YEAR, 2016);
@@ -129,6 +135,7 @@ public class Backend {
         p7.setAuthor("lacajadeldiablo");
         p7.setThumbnail("https://b.thumbs.redditmedia.com/N2rp1KaGV4_jKrqXbMIcfAt_C3Lq9r7w_TKQed-3dGQ.jpg");
         p7.setCommentNumber(735);
+        p7.setId("57jqxr");
 
         // Configuring date for this particular post
         calendar.set(Calendar.YEAR, 2016);
@@ -146,6 +153,7 @@ public class Backend {
         p8.setAuthor("lacajadeldiablo");
         p8.setThumbnail("https://b.thumbs.redditmedia.com/JZUrEDtmUD-grkiTYaeBNagRv0nkJUfIZirHqdPdNxI.jpg");
         p8.setCommentNumber(735);
+        p8.setId("57ll0g");
 
         // Configuring date for this particular post
         calendar.set(Calendar.YEAR, 2016);
@@ -163,6 +171,7 @@ public class Backend {
         p9.setAuthor("lacajadeldiablo");
         p9.setThumbnail("https://b.thumbs.redditmedia.com/RdFk7t4UVA7qD6tz4hgLHUIc3Vs_PRRVXuYMbtSepbA.jpg");
         p9.setCommentNumber(735);
+        p9.setId("57lsmb");
 
         // Configuring date for this particular post
         calendar.set(Calendar.YEAR, 2016);
@@ -180,7 +189,8 @@ public class Backend {
         p10.setAuthor("lacajadeldiablo");
         p10.setThumbnail("https://a.thumbs.redditmedia.com/NiJfvadQD01rkYRi2h1HstxhqnTPia-f128DkDiyOY8.jpg");
         p10.setCommentNumber(735);
-
+        p10.setId("57n1qn");
+        
         // Configuring date for this particular post
         calendar.set(Calendar.YEAR, 2016);
         calendar.set(Calendar.MONTH, Calendar.OCTOBER);
