93d96d275894c3d3f42bfd492f9bcfc32fcaa278
aguilarjp <aguilarjp.95@gmail.com>
2016-10-15 13:17:04 -0300

    Change PostModel thumbnail attribute type

--- app/src/main/java/ar/edu/unc/famaf/redditreader/model/PostModel.java
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/model/PostModel.java
@@ -8,13 +8,13 @@ public class PostModel {
     private String mAuthor;
     private Date mDate;
     private int mCommentNumber;
-    private int mThumbnail; // This is the image URL
+    private String mThumbnail; // This is the image URL
     private String mSubreddit;
 
     public PostModel() {
     }
 
-    public PostModel(String title, String author, Date date, int commentNumber, int thumbnail, String subreddit) {
+    public PostModel(String title, String author, Date date, int commentNumber, String thumbnail, String subreddit) {
         mTitle = title;
         mAuthor = author;
         mDate = date;
@@ -55,11 +55,11 @@ public class PostModel {
         mCommentNumber = commentNumber;
     }
 
-    public int getThumbnail() {
+    public String getThumbnail() {
         return mThumbnail;
     }
 
-    public void setThumbnail(int thumbnail) {
+    public void setThumbnail(String thumbnail) {
         mThumbnail = thumbnail;
     }
 
