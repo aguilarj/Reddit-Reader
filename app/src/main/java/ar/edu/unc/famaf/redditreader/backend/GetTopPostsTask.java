8a0b347872ae50299b7cd226fe84b37a25e99eb4
aguilarjp <aguilarjp.95@gmail.com>
2016-10-29 18:08:16 -0300

    Create a task to download top posts JSON

---
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/backend/GetTopPostsTask.java
@@ -0,0 +1,39 @@
+package ar.edu.unc.famaf.redditreader.backend;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import ar.edu.unc.famaf.redditreader.model.Listing;
+
+/**
+ * Created by aguilarjp on 27/10/16.
+ */
+
+public class GetTopPostsTask extends AsyncTask<URL, Integer, Listing> {
+
+    @Override
+    protected Listing doInBackground(URL... params) {
+        InputStream inputStream = null;
+        try {
+            HttpURLConnection conn = (HttpURLConnection) params[0].openConnection();
+            conn.setRequestMethod("GET");
+            inputStream = conn.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Listing listing = null;
+
+        try {
+            listing = new Parser().readJsonStream(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return listing;
+    }
+}
