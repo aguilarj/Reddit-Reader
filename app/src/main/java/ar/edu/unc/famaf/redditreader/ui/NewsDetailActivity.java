d7fdef84f221304e3612448fca3ff3f57f422c20
aguilarjp <aguilarjp.95@gmail.com>
2016-11-25 13:52:12 -0300

    Complete communication between Activities and Activity-Fragment

--- app/src/main/java/ar/edu/unc/famaf/redditreader/ui/NewsDetailActivity.java
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/ui/NewsDetailActivity.java
@@ -1,15 +1,29 @@
 package ar.edu.unc.famaf.redditreader.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import ar.edu.unc.famaf.redditreader.R;
 
 public class NewsDetailActivity extends AppCompatActivity {
+    public final static String EXTRA_POST = "ar.edu.unc.famaf.redditreader.ui.post";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
+
+        Intent intent = this.getIntent();
+
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(EXTRA_POST, intent.getSerializableExtra(EXTRA_POST));
+
+        NewsDetailActivityFragment newsDetailActivityFragment = new NewsDetailActivityFragment();
+        newsDetailActivityFragment.setArguments(bundle);
+
+        getSupportFragmentManager().beginTransaction()
+                                   .add(R.id.post_detail_container, newsDetailActivityFragment)
+                                   .commit();
     }
 }
--- app/src/main/java/ar/edu/unc/famaf/redditreader/ui/NewsDetailActivityFragment.java
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/ui/NewsDetailActivityFragment.java
@@ -6,8 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import ar.edu.unc.famaf.redditreader.R;
+import ar.edu.unc.famaf.redditreader.model.PostModel;
 
 public class NewsDetailActivityFragment extends Fragment {
 
@@ -21,7 +27,22 @@ public class NewsDetailActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news_detail_activity, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_news_detail_activity, container, false);
+
+        PostModel post = (PostModel) getArguments().getSerializable(NewsDetailActivity.EXTRA_POST);
+
+        TextView author = (TextView) rootview.findViewById(R.id.post_detail_author);
+        TextView title = (TextView) rootview.findViewById(R.id.post_detail_title);
+        TextView subreddit = (TextView) rootview.findViewById(R.id.post_detail_subreddit);
+        TextView created_time = (TextView) rootview.findViewById(R.id.post_detail_created_time);
+
+        author.setText(post.getAuthor());
+        title.setText(post.getTitle());
+        subreddit.setText(post.getSubreddit());
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+        created_time.setText(sdf.format(new Date(post.getCreatedTime())));
+
+        return rootview;
     }
 
 }
